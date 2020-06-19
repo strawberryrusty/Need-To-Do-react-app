3953fd1ee4a024ecdea9a53dbbe671d55ccac293
Prash Mohan <prashmohanuon@gmail.com>
2020-06-19 21:26:35 +0100

    added input field with JSX

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React, {Component} from 'react';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state ={
+      newItem: '',
+      list: []
+    }
+  }
+
   render() {
     return (
       <div className="App">
-      Hello World
+        <div>
+         Add an item...
+         <br/>
+         <input
+            type="text"
+            placeholder="Type item here"
+            value={this.state.newItem}
+            onChange={e => this.updateInput("newItem", e.target.value)}
+          />
+        </div>
       </div>
     )
   }
