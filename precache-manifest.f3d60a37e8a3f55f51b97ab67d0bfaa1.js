5d4150f8fbdcf31366e381ecfa78d2df01c51182
Prash Mohan <prashmohanuon@gmail.com>
2020-06-23 00:22:56 +0100

    Updates

---
+++ precache-manifest.f3d60a37e8a3f55f51b97ab67d0bfaa1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a45d6b66eced6d24109eecba2d7eb87f",
+    "url": "/Need-To-Do-react-app/index.html"
+  },
+  {
+    "revision": "a8291189324dd5b38815",
+    "url": "/Need-To-Do-react-app/static/css/main.b1939753.chunk.css"
+  },
+  {
+    "revision": "af53db3969fa21ac83ab",
+    "url": "/Need-To-Do-react-app/static/js/2.4cf9e0c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Need-To-Do-react-app/static/js/2.4cf9e0c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8291189324dd5b38815",
+    "url": "/Need-To-Do-react-app/static/js/main.073ae944.chunk.js"
+  },
+  {
+    "revision": "02f796febf9f2748b73e",
+    "url": "/Need-To-Do-react-app/static/js/runtime-main.1adc5614.js"
+  }
+]);
